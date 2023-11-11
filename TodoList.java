26b236c31539e954b54087497e1c8debe7bd4d2c
osmanseytablaev <osman.seytablaev@gmail.com>
2023-11-11 14:37:46 +0000

    Label

--- TodoList.java
+++ TodoList.java
@@ -1,7 +1,12 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import javafx.scene.control.Label;
 
 public class TodoList extends Application {
 
@@ -10,10 +15,28 @@ public class TodoList extends Application {
     }
 
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("To-Do App");
+        primaryStage.setTitle("To-Do List App");
 
-        StackPane root = new StackPane();
-        primaryStage.setScene(new Scene(root, 800, 500));
+        StackPane stackPane = new StackPane();
+
+        Rectangle redRectangle = new Rectangle(400, 300);
+        redRectangle.setFill(Color.DARKBLUE);
+        redRectangle.setArcWidth(20);
+        redRectangle.setArcHeight(20);
+
+        stackPane.setStyle("-fx-background-color: lightblue;");
+
+        Label label = new Label("To-Do List App");
+        label.setFont(new Font(50));
+        label.setTextFill(Color.WHITE);
+
+        stackPane.getChildren().add(redRectangle);
+        stackPane.getChildren().add(label);
+
+        StackPane.setAlignment(label, javafx.geometry.Pos.TOP_CENTER);
+
+        Scene myScene = new Scene(stackPane, 800, 500);
+        primaryStage.setScene(myScene);
 
         primaryStage.show();
     }
